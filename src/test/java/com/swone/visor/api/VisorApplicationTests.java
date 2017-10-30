93900dedd08f800e7ad7a90fda71b9ce844b0028
Julio César Back <julio.c@swone.com.br>
2017-10-30 20:49:56 -0200

    Commit inicial dos arquivos

---
+++ src/test/java/com/swone/visor/api/VisorApplicationTests.java
@@ -0,0 +1,16 @@
+package com.swone.visor.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VisorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
